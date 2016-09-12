a037b0aa2dcfbe1e0467cb61a59dce0bb02a0788
aferreira <aferreira@MJV-000361.MJV.net>
2016-09-12 00:00:03 -0300

    ..

---
+++ src/main/java/com/algaworks/brewer/controller/EstiloController.java
@@ -0,0 +1,42 @@
+package com.algaworks.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.Estilo;
+
+
+@Controller
+public class EstiloController {
+	
+	
+	@RequestMapping("/estilos/novo")
+	public String novo(Estilo estilo) {
+		//model.addAttribute(new estilo());
+		return "estilo/CadastroEstilo";
+	}
+	
+	
+	@RequestMapping(value = "/estilos/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Estilo estilo, BindingResult result,Model model, RedirectAttributes attributes){
+		
+		if (result.hasErrors()) {
+			//model.addAttribute(estilo);
+			return novo(estilo);
+			
+		}
+		attributes.addFlashAttribute("mensagem", "estilo salva com sucesso!");
+		return "redirect:/estilos/novo";
+	}
+	
+	
+	
+	
+
+}
